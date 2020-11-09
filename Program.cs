7be8a1dbeccb6635f19209b612f7a05515c91415
Juwizzle <juwfas@gmail.com>
2020-11-10 00:30:22 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook_lIkes_31st_Oct_2020_Chapter_6_Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Input the names of babes");
+            var inputBabesname = Console.ReadLine();
+            string[] words = inputBabesname.Split(',');
+            if (words.Length == 1)
+            {
+                Console.WriteLine(words[0] + " Likes your post ");
+            }
+            if(words.Length == 2)
+            {
+                Console.WriteLine(words[0] + " , " + words[1] + " Likes your post");
+            }
+            if (words.Length > 2)
+            {
+                int others = words.Length - 2;
+                string
+                    values = others > 1 ? "others" : "other";
+                Console.WriteLine(words[0] + "," + words[1] + " and " + others.ToString()  + " " + values  + " Likes your post");
+            }
+        }
+    }
+}
